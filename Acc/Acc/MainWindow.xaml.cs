83660d0c1fc3caca40007372062271e9031206b5
sergey <sergey.pyrkin@instatsport.com>
2020-11-11 11:01:02 +0300

    first

---
+++ Acc/Acc/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Acc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void add_game(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void add_players(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void game_list(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
